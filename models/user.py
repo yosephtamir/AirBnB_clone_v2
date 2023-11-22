df91b05e7a4ca6f6864ae15d89c6b10a80e47a3e
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-11-22 06:41:53 -0800

    Commit

--- models/user.py
+++ models/user.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel,Base
+from models import storage_type
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
