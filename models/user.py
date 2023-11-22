bdeeee0d1aa47b42657b806e3ee8376d9f12d207
Yoseph Tamirat <yosephtamir@gmail.com>
2023-11-22 21:55:20 +0300

    fle

--- models/user.py
+++ models/user.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel,Base
-from models import storage_type
+from models.base_model import BaseModel, Base
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes
     email = string
     password = password
@@ -16,18 +17,18 @@ class User(BaseModel):
     reviews: relationship
     reviews: relationship
     """
-     __tablename__ = 'users'
-     if storage_type == 'db':
-         email = Column(String(128), nullable=False)
-         password = Column(String(128),nullable=False)
-         first_name = Column(String(128), nullable=True)
-         last_name = Column(String(128), nullable=True)
-         places = relationship('Place', backref='user',
-                 cascade='all, delete, delete-orphan')
-         reviews = relationship('Review', backref='user',
-                 cascade='all, delete, delete-orphan')
-     else:
-         email = ""
-         password = ""
-         first_name = ""
-         last_name = ""
+    __tablename__ = 'users'
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
