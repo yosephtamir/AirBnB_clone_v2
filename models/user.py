2dee356f2630ab988dbdd8d45dded2d269b93000
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-11-22 06:36:27 -0800

    Commit

--- models/user.py
+++ models/user.py
@@ -4,8 +4,27 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """This class defines a user by various attributes
+    email = string
+    password = password
+    first_name = name
+    last_name = name
+    places: relationship
+    reviews: relationship
+    reviews: relationship
+    """
+     __tablename__ = 'users'
+     if storage_type == 'db':
+         email = Column(String(128), nullable=False)
+         password = Column(String(128),nullable=False)
+         first_name = Column(String(128), nullable=True)
+         last_name = Column(String(128), nullable=True)
+         places = relationship('Place', backref='user',
+                 cascade='all, delete, delete-orphan')
+         reviews = relationship('Review', backref='user',
+                 cascade='all, delete, delete-orphan')
+     else:
+         email = ""
+         password = ""
+         first_name = ""
+         last_name = ""
