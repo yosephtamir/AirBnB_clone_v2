7c4339b0068abe4e71aff0403eb041fc11b51194
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-26 11:30:27 +0300

    a Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -17,4 +17,4 @@ def hello():
 
 if __name__ == "__main__":
     """This script runs if it is not imported"""
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
