5870bd1904584d83095ab3e0e9c83f4b7723b34b
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-26 11:29:33 +0300

    a Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+This Module is used to serve a web app on host = 0.0.0.0 and port = 5000
+     it serves at the root only
+"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
+    """This is the main entery of the host"""
     return "Hello HBNB"
 
+
 if __name__ == "__main__":
+    """This script runs if it is not imported"""
     app.run(debug=True, host='0.0.0.0', port=5000)
